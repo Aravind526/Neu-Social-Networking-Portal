2849f5478d3d413bd957041cf9cedd678f0b15cd
Aravind526 <aravind526071@gmail.com>
2025-05-29 13:37:35 -0500

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://github.com/Aravind526/Neu-Social-Networking-Portal.git',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
